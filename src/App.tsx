e1a4a5549fec28f7d09436e2bf10eda93a71c25d
Atticus <atticusofsparta@protonmail.com>
2022-10-29 11:26:42 -0500

    chore(prettier): linting

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
-import { Routes, Route, BrowserRouter as Router } from 'react-router-dom';import './index.css';
+import { Routes, Route, BrowserRouter as Router } from 'react-router-dom';
+import './index.css';
 import Home from './components/pages/Home/Home';
 
 function App() {
   return (
     <div>
       <Routes>
-      <Route path="/" element={<Home />} />
+        <Route path="/" element={<Home />} />
         <Route path="*" element={<Home />} />
-        
       </Routes>
     </div>
   );
