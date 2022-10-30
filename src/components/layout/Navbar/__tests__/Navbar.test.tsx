d5e22c9ab3e64b5fa839e06a74f12aa63380e49c
dtfiedler <dylan@ar.io>
2022-10-30 15:45:13 -0600

    chore(jest): add gifs to stubbed files in jest config, move assets to top level

--- src/components/layout/Navbar/__tests__/Navbar.test.tsx
+++ src/components/layout/Navbar/__tests__/Navbar.test.tsx
@@ -1,10 +1,15 @@
 import { render, cleanup } from '@testing-library/react';
+import { BrowserRouter as Router } from 'react-router-dom';
 import Navbar from '../Navbar';
 
 describe('Navbar', () => {
   afterEach(cleanup);
 
   test('render Navbar', () => {
-    render(<Navbar />);
+    render(
+      <Router>
+        <Navbar />
+      </Router>,
+    );
   });
 });
