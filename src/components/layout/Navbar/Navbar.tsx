6b6f0c8d2f4b82d908a5a5b017883f1f891caeb0
Atticus <atticusofsparta@protonmail.com>
2022-10-31 10:48:25 -0600

    bug(build): temporary fix for import issue on logo gif. Should come back and fix the import issue instead of hard-coding path

--- src/components/layout/Navbar/Navbar.tsx
+++ src/components/layout/Navbar/Navbar.tsx
@@ -1,12 +1,18 @@
 import { Link } from 'react-router-dom';
-import * as logo from '../../../../assets/images/logo/winston-white.gif';
+//import * as logo from '../../../../assets/images/logo/winston-white.gif';
+// TODO: the above import causes the error 'Type 'typeof import("*.gif")' is not assignable to type 'string'.'
+// we should fix this instead of hard coding the path in the img src
 
 function NavBar() {
   return (
     <div className="navBar">
       <div className="navBarItemContainer">
         <Link to="/" className="brandLogo">
-          <img src={logo} width={90} height={90} />
+          <img
+            src={'../../../../assets/images/logo/winston-white.gif'}
+            width={90}
+            height={90}
+          />
         </Link>
       </div>
     </div>
