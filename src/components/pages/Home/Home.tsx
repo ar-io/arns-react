225cedea98da9801936cbcea7893236128626076
dtfiedler <dylan@ar.io>
2022-11-02 17:32:20 -0600

    chore: reduce scope, just interface for now

--- src/components/pages/Home/Home.tsx
+++ src/components/pages/Home/Home.tsx
@@ -1,40 +1,7 @@
-import { useEffect, useState } from 'react';
-import { LocalFileSystemDataProvider } from '../../../services/arweave/LocalFilesystemDataProvider';
 import './styles.css';
 
-const ARNS_SOURCE_CONTRACT_TX_ID =
-  'bLAgYxAdX2Ry-nt6aH2ixgvJXbpsEYm28NgJgyqfs-U';
-
 function Home() {
-  const [arnsRecords, setArnsRecords] = useState('');
-  useEffect(() => {
-    // TODO: might be beneficial to move this to a web worker
-    const fetchArNSContract = async (txId: string): Promise<Map<string, string>> => {
-      const arnsContractState =
-        await new LocalFileSystemDataProvider().getContractState(txId);
-      if (!arnsContractState) {
-        throw Error('State failed to load.');
-      }
-      const { records } = arnsContractState;
-      return records;
-    };
-    fetchArNSContract(ARNS_SOURCE_CONTRACT_TX_ID)
-      .then((records) => {
-        setArnsRecords(JSON.stringify(records, null, 2));
-      })
-      .catch((error) => {
-        console.log(error.message);
-      });
-  }, [arnsRecords]);
-
-  return (
-    <div className="home">
-      Home
-      <div className="container">
-        <pre>{arnsRecords}</pre>
-      </div>
-    </div>
-  );
+  return <div className="home">Home</div>;
 }
 
 export default Home;
