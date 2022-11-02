d1b4fb71ffb8c9221656f6356f62faebdee60fdb
dtfiedler <dylan@ar.io>
2022-11-02 14:18:00 -0600

    chore: fix tests

--- src/services/arweave/LocalFilesystemDataProvider.ts
+++ src/services/arweave/LocalFilesystemDataProvider.ts
@@ -1,13 +1,10 @@
-import { SmartweaveContractSource, ArNSContractState } from '../types.js';
+import { SmartweaveContractSource, ArNSContractState } from '../types';
 import axios from 'axios';
 
 export class LocalFileSystemDataProvider implements SmartweaveContractSource {
   async getContractState(txId: string): Promise<ArNSContractState | undefined> {
     try {
-      const localFile = new URL(
-        `../../../data/contracts/${txId}.json`,
-        import.meta.url,
-      ).href;
+      const localFile = `../../../data/contracts/${txId}.json`;
       const { data } = await axios.get(localFile);
       const arnsContractState = data as ArNSContractState;
       return arnsContractState;
