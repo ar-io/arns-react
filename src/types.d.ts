9511b6f6bbc744bcbf59b21b3b6615b32adcce68
Atticus <atticusofsparta@protonmail.com>
2022-11-03 13:05:54 -0600

    chore(types): fix types

--- src/types.d.ts
+++ src/types.d.ts
@@ -15,10 +15,3 @@ export type searchBar = {
   searchButtonAction: any;
 };
 
-declare module '*.svg' {
-  import React = require('react');
-  export const ReactComponent: React.FC<React.SVGProps<SVGSVGElement>>;
-  const src: string;
-  export default src;
-}
- 
