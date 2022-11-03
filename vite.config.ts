10e3f1186f957fd78f321ed39f6f74b589474587
Atticus <atticusofsparta@protonmail.com>
2022-11-03 12:51:03 -0600

    chore(vite): fix base pat in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,6 @@ import svgr from 'vite-plugin-svgr';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svgr(), react()],
+    // TODO: remove this when no longer deploying to GH pages
+    base: '/arns-react/'
 });
