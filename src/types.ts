56b818ebeb8fcbe4e6c68f6588c6116bbef40ea6
dtfiedler <dylan@ar.io>
2022-11-03 13:03:55 -0600

    fix: fix typings and remove regex type for now due to lint errors

---
+++ src/types.ts
@@ -0,0 +1,18 @@
+export type ArNSContractState = {
+  records: Map<string, ArweaveTransactionId>;
+};
+
+// TODO: match this to a regex
+export type ArweaveTransactionId = string;
+
+export interface SmartweaveContractSource {
+  getContractState(
+    contractId: ArweaveTransactionId,
+  ): Promise<ArNSContractState | undefined>;
+}
+
+export type searchBar = {
+  searchButtonAction: any;
+};
+
+ 
