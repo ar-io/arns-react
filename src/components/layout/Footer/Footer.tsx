9d7bcbcef7b5e7355bc48b673a7ad84be11aadfa
dtfiedler <dylan@ar.io>
2022-11-04 16:14:43 -0600

    chore: display app version in footer

--- src/components/layout/Footer/Footer.tsx
+++ src/components/layout/Footer/Footer.tsx
@@ -1,7 +1,13 @@
 function Footer() {
   return (
     <div>
-      <span className="textFaded">Copyright ar.io 2022</span>
+      {/* @ts-ignore */}
+      <div className="textFaded">
+        v{APP_VERSION}
+        <br />
+        <br />
+        Copyright ar.io 2022
+      </div>
     </div>
   );
 }
