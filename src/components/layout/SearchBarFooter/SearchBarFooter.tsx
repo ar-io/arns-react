88179329c8c52988804bec84ebbad7cd87b67360
Atticus <atticusofsparta@protonmail.com>
2022-11-11 15:27:24 -0600

    added search regex pr

--- src/components/layout/SearchBarFooter/SearchBarFooter.tsx
+++ src/components/layout/SearchBarFooter/SearchBarFooter.tsx
@@ -1,16 +1,28 @@
 import { SearchBarFooterProps } from '../../../types';
 import AntCard from '../../cards/AntCard/AntCard';
+import FeaturedDomains from '../FeaturedDomains/FeaturedDomains';
 import './styles.css';
 import './styles.css';
 
 function SearchBarFooter({
   defaultText,
   searchResult,
+  isSearchValid
 }: SearchBarFooterProps): JSX.Element {
   return (
     <>
-      {!searchResult?.id ? (
-        <div className="textFaded">{defaultText}</div>
+      {!searchResult?.id ? (<>
+        <div className="text faded">
+          {!isSearchValid ? (
+            <div className="errorContainer">
+              <span className="illegalChar">{defaultText}</span>
+            </div>
+          ) : (
+            defaultText
+          )}
+        </div>
+        <FeaturedDomains />
+        </>
       ) : (
         <AntCard contract={searchResult} />
       )}
