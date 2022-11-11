3763a3dd9d36632263b5680a46a2997e37e5cf9a
dtfiedler <dylan@ar.io>
2022-11-11 10:20:39 -0700

    chore: break out available vs valid

--- src/utils/searchUtils.ts
+++ src/utils/searchUtils.ts
@@ -1,5 +1,16 @@
 import { ARNS_NAME_REGEX } from '../../types/constants';
 
+export function isArNSDomainNameValid({
+  name,
+}: {
+  name: string | undefined;
+}): boolean | undefined {
+  // if name is not in the legal character range or chars, return undefined
+  if (!name || !ARNS_NAME_REGEX.test(name)) {
+    return false;
+  }
+}
+
 export function isArNSDomainNameAvailable({
   name,
   records,
@@ -7,16 +18,9 @@ export function isArNSDomainNameAvailable({
   name: string;
   records: Record<string, any>;
 }): boolean | undefined {
-  // if name is not in the legal character range or chars, return undefined
-  if (!ARNS_NAME_REGEX.test(name) && name !== '') {
-    return false;
-  }
-  //if not registered return true
-  if (!Object.keys(records).includes(name) && name !== '') {
-    return true;
-  }
   //if registered return false
   if (Object.keys(records).includes(name) && name !== '') {
     return false;
   }
+  return true;
 }
