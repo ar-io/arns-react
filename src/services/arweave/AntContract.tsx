fe4b10cbf4c1af635a24b64def840c6a59dab185
atticusofsparta <atticusofsparta@protonmail.com>
2023-02-28 16:14:31 -0600

    chore(pr recs)

--- src/services/arweave/AntContract.tsx
+++ src/services/arweave/AntContract.tsx
@@ -5,178 +5,109 @@ import {
   SmartweaveDataProvider,
 } from '../../types';
 
-export class AntContract {
+export const DEFAULT_ANT_CONTRACT_STATE: ANTContractState = {
+  balances: {},
+  evolve: undefined,
+  name: '',
+  ticker: '',
+  owner: '',
+  controllers: [],
+  records: {
+    '@': {
+      transactionId: '',
+      ttlSeconds: 1800,
+      maxSubdomains: 100,
+    },
+  },
+};
+
+export class CompositeAntContractState {
   private _provider: SmartweaveDataProvider & ArweaveDataProvider;
-  state?: ANTContractState = undefined;
-  antId;
-  walletAddress: ArweaveTransactionID;
-  newAntState: ANTContractState;
-  /**
-   * AntContractProvider
-   * @param walletAddress - @type {ArweaveTransactionID} - connected wallet address
-   * @param provider -  @type {ArweaveCompositeDataProvider}
-   * @param antId - @type {ArweaveTransactionID | undefined} -
-   * ANT ID to use for loading a pre-existing ANT's contract state. If left undefined, default new ANT state is loaded
-   */
+  contract?: AntContractState;
+  antId?: string;
+
   constructor(
-    walletAddress: ArweaveTransactionID,
+    contract: AntContractState,
     provider: SmartweaveDataProvider & ArweaveDataProvider,
-    antId?: ArweaveTransactionID,
   ) {
-    this.walletAddress = walletAddress;
-    this.antId = antId;
+    (this.contract = contract), (this.antId = contract.antId);
     this._provider = provider;
-    this.newAntState = {
-      balances: {
-        [this.walletAddress.toString()]: 1,
-      },
-      evolve: undefined,
-      name: '',
-      ticker: '',
-      owner: this.walletAddress.toString(),
-      controllers: [this.walletAddress.toString()],
-      records: {
-        '@': {
-          transactionId: '',
-          ttlSeconds: 0,
-          maxSubdomains: 100,
-        },
-      },
-    };
-    this.loadState(antId);
   }
 
-  getCurrentState() {
-    return this.state;
-  }
-
-  /**
-   * loadState
-   * @param contractId @type {ArweaveTransactionID | undefined} -
-   * ANT contract id to load, if undefined default new ANT state is loaded.
-   * @param overwrite @type {boolean | undefined} -
-   * if true allows instance state to be overwritten with a new ANT contract state.
-   * Does not affect the antId state of the instance. Usefull for cloning or bootstrapping a pre-existing ANT.
-   * @returns void
-   */
-  async loadState(contractId?: ArweaveTransactionID, overwrite?: boolean) {
+  async setOwner(id: ArweaveTransactionID) {
     try {
-      if (this.state && !overwrite) {
+      if (!this.antId) {
         throw new Error(
-          `Could not overwrite state: Enable overwrite to load a new state while a current state is loaded.
-                   Current ANT:  ${{
-                     id: this.antId?.toString(),
-                     name: this.state.name,
-                   }}`,
-        );
-      }
-
-      if (!contractId) {
-        this.state = this.newAntState;
-        return;
-      }
-      const state: ANTContractState = await this._provider.getContractState(
-        contractId,
-      );
-      if (!state) {
-        throw Error(`Could not fetch state for ${contractId.toString()}`);
-      }
-      return (this.state = state);
-    } catch (error) {
-      console.error(error);
-      if (!this.state) {
-        console.log(
-          'unable to load provided antId: loading default new ANT state.',
-        );
-        this.state = this.newAntState;
-      }
-
-      return;
-    }
-  }
-
-  async setOwner(id: ArweaveTransactionID, update?: boolean) {
-    try {
-      if (!this.state) {
-        throw new Error(
-          `Cannot set state items when state is not loaded. Current state: ${this.state}`,
+          `Cannot set state items when id is not loaded. Current id: ${this.antId}`,
         );
       }
       if (!id) {
         throw new Error(`type of id <${id}> is not a  valid id.`);
       }
-      this.state.owner = id.toString();
-      this.state.balances = { ...this.state.balances, [id.toString()]: 1 };
-      if (update === true && this.antId) {
-        const payload = {
-          function: 'transfer',
-          target: id.toString(),
-        };
-        const txid = await this._provider.writeTransaction(this.antId, payload);
-        if (!txid) {
-          throw new Error(`Failed to transfer ANT token`);
-        }
-        return txid;
+
+      const payload = {
+        function: 'transfer',
+        target: id.toString(),
+      };
+      const txid = await this._provider.writeTransaction(
+        new ArweaveTransactionID(this.antId),
+        payload,
+      );
+      if (!txid) {
+        throw new Error(`Failed to transfer ANT token`);
       }
-      return;
+      return txid;
     } catch (error) {
       console.error(error);
       return;
     }
   }
 
-  async setController(id: ArweaveTransactionID, update?: boolean) {
+  async setController(id: ArweaveTransactionID) {
     try {
-      if (!this.state) {
-        throw new Error(
-          `Cannot set state items when state is not loaded. Current state: ${this.state}`,
-        );
+      if (typeof this.antId !== 'string') {
+        throw new Error('No Contract id Defined');
       }
-      if (!id) {
-        throw new Error(`type of id <${id}> is not a valid id.`);
+
+      const payload = {
+        function: 'setController',
+        target: id.toString(),
+      };
+      const txid = await this._provider.writeTransaction(
+        new ArweaveTransactionID(this.antId),
+        payload,
+      );
+      if (!txid) {
+        throw new Error('Failed to add controller to contract');
       }
-      this.state.controllers = [...this.state.controllers, id.toString()];
-      if (update === true && this.antId) {
-        const payload = {
-          function: 'setController',
-          target: id.toString(),
-        };
-        const txid = await this._provider.writeTransaction(this.antId, payload);
-        if (!txid) {
-          throw new Error('Failed to add controller to contract');
-        }
-        return txid;
-      }
-      return;
+      return txid;
     } catch (error) {
       console.error(error);
       return;
     }
   }
-  async setTargetId(id: ArweaveTransactionID, update?: boolean) {
+  async setTargetId(id: ArweaveTransactionID) {
     try {
-      if (!this.state) {
-        throw new Error(
-          `Cannot set state items when state is not loaded. Current state: ${this.state}`,
-        );
+      if (typeof this.antId !== 'string') {
+        throw new Error('No Contract id Defined');
       }
       if (!id) {
         throw new Error(`type of id <${id}> is not a valid id.`);
       }
-      this.state.records['@'].transactionId = id.toString();
-      if (update === true && this.antId) {
-        const payload = {
-          function: 'setRecord',
-          transactionId: id.toString(),
-          subDomain: '@',
-        };
-        const txid = await this._provider.writeTransaction(this.antId, payload);
-        if (!txid) {
-          throw new Error('Failed to write target ID to contract');
-        }
-        return txid;
+
+      const payload = {
+        function: 'setRecord',
+        transactionId: id.toString(),
+        subDomain: '@',
+      };
+      const txid = await this._provider.writeTransaction(
+        new ArweaveTransactionID(this.antId),
+        payload,
+      );
+      if (!txid) {
+        throw new Error('Failed to write target ID to contract');
       }
-      return;
+      return txid;
     } catch (error) {
       console.error(error);
       return;
@@ -195,62 +126,46 @@ export class AntContract {
     targetId,
     maxSubdomains,
     ttl,
-    update,
   }: {
     name: string;
     targetId: ArweaveTransactionID | string;
     maxSubdomains?: number;
     ttl?: number;
-    update?: boolean;
   }) {
     try {
-      if (!this.state) {
-        throw new Error(
-          `Cannot set state items when state is not loaded. Current state: ${this.state}`,
-        );
+      if (typeof this.antId !== 'string') {
+        throw new Error('No Contract id Defined');
+      }
+      if (!this.contract) {
+        throw new Error('No Contract is Defined');
       }
       if (!targetId) {
         throw new Error(`type of id <${typeof targetId}> is not a valid id.`);
       }
-      this.state.records = {
-        ...this.state.records,
-        [name]: {
-          ttlSeconds: ttl
-            ? ttl
-            : this.state.records[name].ttlSeconds
-            ? this.state.records[name].ttlSeconds
-            : 1800,
-          maxSubdomains: maxSubdomains
-            ? maxSubdomains
-            : this.state.records[name].maxSubdomains
-            ? this.state.records[name].maxSubdomains
-            : 100,
-          transactionId: targetId.toString(),
-        },
+
+      const payload = {
+        function: 'setRecord',
+        subDomain: name,
+        target: targetId.toString(),
+        ttlSeconds: ttl
+          ? ttl
+          : this.contract.state.records[name].ttlSeconds
+          ? this.contract.state.records[name].ttlSeconds
+          : 1800,
+        maxSubdomains: maxSubdomains
+          ? maxSubdomains
+          : this.contract.state.records[name].maxSubdomains
+          ? this.contract.state.records[name].maxSubdomains
+          : 100,
       };
-      if (update === true && this.antId) {
-        const payload = {
-          function: 'setRecord',
-          subDomain: name,
-          target: targetId.toString(),
-          ttlSeconds: ttl
-            ? ttl
-            : this.state.records[name].ttlSeconds
-            ? this.state.records[name].ttlSeconds
-            : 1800,
-          maxSubdomains: maxSubdomains
-            ? maxSubdomains
-            : this.state.records[name].maxSubdomains
-            ? this.state.records[name].maxSubdomains
-            : 100,
-        };
-        const txid = await this._provider.writeTransaction(this.antId, payload);
-        if (!txid) {
-          throw new Error('Failed to write undername to contract');
-        }
-        return txid;
+      const txid = await this._provider.writeTransaction(
+        new ArweaveTransactionID(this.antId),
+        payload,
+      );
+      if (!txid) {
+        throw new Error('Failed to write undername change to ANT');
       }
-      return;
+      return txid;
     } catch (error) {
       console.error(error);
       return;
@@ -260,40 +175,88 @@ export class AntContract {
    * @param name {string} name - undername to remove from ANT state
    * @returns void - Function alters instance state and returns void.
    */
-  async removeUndername(name: string, update?: boolean) {
+  async removeUndername(name: string) {
     try {
-      if (!this.state) {
-        throw new Error(
-          `Cannot set state items when state is not loaded. Current state: ${this.state}`,
-        );
+      if (typeof this.antId !== 'string') {
+        throw new Error('No Contract id Defined');
       }
       if (!name) {
         throw new Error(`type of id <${name}> is not a valid id.`);
       }
-      delete this.state?.records[name];
-      if (update === true && this.antId) {
-        const payload = {
-          function: 'setRecord',
-          subDomain: name,
-        };
-        const txid = await this._provider.writeTransaction(this.antId, payload);
-        if (!txid) {
-          throw new Error('Failed to remove undername from contract');
-        }
-        return txid;
+
+      const payload = {
+        function: 'setRecord',
+        subDomain: name,
+      };
+      const txid = await this._provider.writeTransaction(
+        new ArweaveTransactionID(this.antId),
+        payload,
+      );
+      if (!txid) {
+        throw new Error('Failed to remove undername from contract');
       }
-      return;
+      return txid;
     } catch (error) {
       console.error(error);
       return;
     }
   }
+}
 
-  async update() {
-    return;
-    // for batch interaction function, could generate a diff of state keys, then write an interaction for each addition.
-    // so check if new owner !== old owner, if not equal, write a newOwner interaction. If an undername was added write that... etc
-    // this would enable the user to batch edit a bunch of keys in the contract, then save the contract state, and the app
-    // would write all the interactions automatically. UI could have an Undo and Reset function to return to previous states
+export class AntContractState {
+  id?: ArweaveTransactionID;
+  contract: ANTContractState;
+  // todo: add last updated - lastUpdated: Map<keyof AntContract, number> = new Map();
+
+  constructor(state?: ANTContractState, id?: ArweaveTransactionID) {
+    this.id = id;
+    if (state) {
+      this.contract = state;
+    } else {
+      this.contract = DEFAULT_ANT_CONTRACT_STATE;
+    }
+  }
+  get owner() {
+    return this.contract.owner;
+  }
+  set owner(id: string) {
+    this.contract.owner = id;
+  }
+  get name() {
+    return this.contract.name;
+  }
+  set name(name: string) {
+    this.contract.name = name;
+  }
+  get ticker() {
+    return this.contract.ticker;
+  }
+  set ticker(ticker: string) {
+    this.contract.ticker = ticker;
+  }
+  get balances() {
+    return this.contract.balances;
+  }
+  set balances(balances: { [x: string]: number }) {
+    this.contract.balances = balances;
+  }
+  get controllers() {
+    return this.contract.controllers;
+  }
+  set controllers(controllers: string[]) {
+    // any validations we want to do on the controller
+    this.contract.controllers = controllers;
+  }
+  get evolve() {
+    return this.contract.evolve;
+  }
+  set evolve(evolve: boolean | undefined) {
+    this.contract.evolve = evolve;
+  }
+  get state() {
+    return this.contract;
+  }
+  get antId(): string | undefined {
+    return this.antId?.toString();
   }
 }
