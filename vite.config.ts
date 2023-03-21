8819072ca30ab2b99476f3ea58e34828dc9199ef
atticusofsparta <atticusofsparta@protonmail.com>
2023-03-21 11:59:45 -0600

    bug(fix):node is undefined

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,18 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+import { nodePolyfills } from 'vite-plugin-node-polyfills';
 import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svgr(), react()],
+  plugins: [
+    svgr(),
+    react(),
+    nodePolyfills({
+      // Whether to polyfill `node:` protocol imports.
+      protocolImports: true,
+    }),
+  ],
   // TODO: remove this when no longer deploying to GH pages
   base: '/arns-react/',
   define: {
