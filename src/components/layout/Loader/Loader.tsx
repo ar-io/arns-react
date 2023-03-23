040e54fbbafa2e0e91c671da4a0af59cfc84ee73
dtfiedler <dylan@ar.io>
2023-03-23 15:36:36 -0600

    chore: update loader component

--- src/components/layout/Loader/Loader.tsx
+++ src/components/layout/Loader/Loader.tsx
@@ -5,16 +5,22 @@ export default function Loader({
   size = 80,
   color = 'white',
   message = undefined,
+  wrapperStyle = {},
 }: {
   size?: number;
   color?: string;
   message?: string;
+  wrapperStyle?: any;
 }) {
   return (
     <Spin
       tip={message}
       style={{ fontSize: '18px', color: color }}
-      indicator={<LoadingOutlined style={{ fontSize: size, margin: '15px' }} />}
+      indicator={
+        <LoadingOutlined
+          style={{ fontSize: size, margin: '15px', ...wrapperStyle }}
+        />
+      }
     />
   );
 }
