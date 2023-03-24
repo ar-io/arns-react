9b8799d076ea12eaaac6f9da2cb300245a728c81
dtfiedler <dylan@ar.io>
2023-03-24 14:37:55 -0600

    chore: remove sentry dsn var

--- src/utils/sentry.ts
+++ src/utils/sentry.ts
@@ -4,7 +4,6 @@ import { BrowserTracing } from '@sentry/tracing';
 const SENTRY_DSN_PUBLIC_KEY = import.meta.env.VITE_SENTRY_DSN_PUBLIC_KEY;
 const SENTRY_DSN_PROJECT_URI = import.meta.env.VITE_SENTRY_DSN_PROJECT_URI;
 const SENTRY_DSN_PROJECT_ID = import.meta.env.VITE_SENTRY_DSN_PROJECT_ID;
-const SENTRY_DSN_ENDPOINT = `https://${SENTRY_DSN_PUBLIC_KEY}@${SENTRY_DSN_PROJECT_URI}/${SENTRY_DSN_PROJECT_ID}`;
 
 export default function setupSentry() {
   if (
@@ -14,7 +13,7 @@ export default function setupSentry() {
   ) {
     console.debug('Setting up Sentry monitoring.');
     Sentry.init({
-      dsn: SENTRY_DSN_ENDPOINT,
+      dsn: `https://${SENTRY_DSN_PUBLIC_KEY}@${SENTRY_DSN_PROJECT_URI}/${SENTRY_DSN_PROJECT_ID}`,
       integrations: [new BrowserTracing()],
       tracesSampleRate: 1.0,
       environment: import.meta.env.VITE_ENVIRONMENT ?? 'develop',
