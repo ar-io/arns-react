905379fe5cffe0568cf94a9d258aad21a172b7cf
dtfiedler <dylan@ar.io>
2023-03-24 11:02:18 -0600

    feat(sentry): setup sentry monitoring

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,10 @@ import App from './App';
 import './index.css';
 import GlobalStateProvider from './state/contexts/GlobalState';
 import { reducer } from './state/reducers/GlobalReducer';
+import setupSentry from './utils/sentry';
+
+// Setup sentry monitoring
+setupSentry();
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_APP_TITLE: string;
+  readonly VITE_SENTRY_DSN_ENDPOINT: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
