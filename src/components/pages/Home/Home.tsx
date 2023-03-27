39a574b058bbf8f0fe20725e2ab853931de144c5
dtfiedler <dylan@ar.io>
2023-03-27 14:10:04 -0600

    fix(PE-3339): block searching until arns contracts are loaded

--- src/components/pages/Home/Home.tsx
+++ src/components/pages/Home/Home.tsx
@@ -10,7 +10,7 @@ import {
   isArNSDomainNameValid,
 } from '../../../utils/searchUtils';
 import SearchBar from '../../inputs/Search/SearchBar/SearchBar';
-import { FeaturedDomains, RegisterNameForm } from '../../layout';
+import { FeaturedDomains, Loader, RegisterNameForm } from '../../layout';
 import { SearchBarFooter, SearchBarHeader } from '../../layout';
 import ConfirmRegistration from '../../layout/ConfirmRegistration/ConfirmRegistration';
 import SuccessfulRegistration from '../../layout/SuccessfulRegistration/SuccessfulRegistration';
@@ -28,163 +28,167 @@ function Home() {
   }>();
 
   useEffect(() => {
-    const newRecords: { [x: string]: string } = Object.keys(
-      arnsSourceContract.records,
-    ).reduce(
-      (allRecords, domain: string) => ({
-        ...allRecords,
-        [domain]: arnsSourceContract.records[domain].contractTxId,
-      }),
-      {},
-    );
-    const featuredDomains = Object.fromEntries(
-      Object.entries(newRecords).filter(([domain]) => {
-        return FEATURED_DOMAINS.includes(domain);
-      }),
-    );
-    setFeaturedDomains(featuredDomains);
-  }, [arnsSourceContract.records, domain, isSearching]);
+    if (Object.keys(arnsSourceContract.records).length) {
+      const featuredDomains = Object.fromEntries(
+        FEATURED_DOMAINS.map((domain: string) =>
+          arnsSourceContract.records[domain].contractTxId
+            ? [domain, arnsSourceContract.records[domain].contractTxId]
+            : [],
+        ).filter((n) => n.length),
+      );
+      setFeaturedDomains(featuredDomains);
+    }
+  }, [arnsSourceContract.records]);
 
   return (
     <div className="page">
       {domain ? <></> : <div className="page-header">Arweave Name System</div>}
-      <Workflow
-        stage={stage}
-        onNext={() => {
-          dispatchRegisterState({
-            type: 'setStage',
-            payload: stage + 1,
-          });
-        }}
-        onBack={() => {
-          if (stage === 0) {
-            dispatchRegisterState({
-              type: 'reset',
-            });
-            return;
-          }
-          dispatchRegisterState({
-            type: 'setStage',
-            payload: stage - 1,
-          });
-        }}
-        stages={{
-          0: {
-            component: (
-              <SearchBar
-                values={arnsSourceContract.records}
-                value={domain}
-                onSubmit={(next = false) => {
-                  dispatchRegisterState({
-                    type: 'setIsSearching',
-                    payload: true,
-                  });
-                  if (next) {
-                    dispatchRegisterState({
-                      type: 'setStage',
-                      payload: stage + 1,
-                    });
-                  }
-                }}
-                onChange={() => {
-                  dispatchRegisterState({
-                    type: 'reset',
-                  });
-                }}
-                onSuccess={(value: string) => {
-                  dispatchRegisterState({
-                    type: 'setDomainName',
-                    payload: value,
-                  });
-                  dispatchRegisterState({
-                    type: 'setAntID',
-                    payload: undefined,
-                  });
-                }}
-                onFailure={(name: string, result?: string) => {
-                  dispatchRegisterState({
-                    type: 'setDomainName',
-                    payload: name,
-                  });
-                  dispatchRegisterState({
-                    type: 'setAntID',
-                    payload: result
-                      ? new ArweaveTransactionID(result)
-                      : undefined,
-                  });
-                }}
-                successPredicate={(value: string | undefined) =>
-                  isArNSDomainNameAvailable({
-                    name: value,
-                    records: arnsSourceContract?.records ?? {},
-                  })
-                }
-                validationPredicate={(value: string | undefined) =>
-                  isArNSDomainNameValid({ name: value })
-                }
-                placeholderText={'Enter a name'}
-                headerElement={
-                  <SearchBarHeader defaultText={'Find a domain name'} />
-                }
-                footerElement={
-                  <SearchBarFooter
-                    searchTerm={domain}
-                    searchResult={
-                      domain && arnsSourceContract.records[domain]
-                        ? new ArweaveTransactionID(
-                            arnsSourceContract.records[domain].contractTxId,
-                          )
-                        : undefined
-                    }
-                    defaultText="Names must be 1-32 characters. Dashes are permitted, but cannot be trailing characters and cannot be used in single character domains."
-                  />
-                }
-                height={45}
-              />
-            ),
-            disableNext: !isSearching,
-            showNext:
-              !!domain &&
-              isArNSDomainNameAvailable({
-                name: domain,
-                records: arnsSourceContract.records,
-              }) &&
-              isArNSDomainNameValid({ name: domain }),
-            showBack: !!domain,
-            requiresWallet: !!domain && !antID,
-          },
-          1: {
-            component: <RegisterNameForm />,
-            showNext: true,
-            showBack: true,
-            disableNext:
-              !antID ||
-              !ARNS_TX_ID_REGEX.test(antID.toString()) ||
-              !walletAddress,
-            requiresWallet: true,
-          },
-          2: {
-            // this component manages buttons itself
-            component: <ConfirmRegistration />,
-            showNext: false,
-            showBack: false,
-            disableNext: !!domain && !!antID && !walletAddress,
-            requiresWallet: true,
-          },
-          3: {
-            component: <SuccessfulRegistration />,
-            showNext: false,
-            showBack: false,
-            disableNext: true,
-            requiresWallet: true,
-          },
-        }}
-      />
-
-      {featuredDomains && !domain ? (
-        <FeaturedDomains domains={featuredDomains} />
+      {!Object.keys(arnsSourceContract.records).length ? (
+        <Loader
+          size={80}
+          wrapperStyle={{ margin: '75px' }}
+          message="Loading ArNS Registry Contract..."
+        />
       ) : (
-        <></>
+        <>
+          <Workflow
+            stage={stage}
+            onNext={() => {
+              dispatchRegisterState({
+                type: 'setStage',
+                payload: stage + 1,
+              });
+            }}
+            onBack={() => {
+              if (stage === 0) {
+                dispatchRegisterState({
+                  type: 'reset',
+                });
+                return;
+              }
+              dispatchRegisterState({
+                type: 'setStage',
+                payload: stage - 1,
+              });
+            }}
+            stages={{
+              0: {
+                component: (
+                  <SearchBar
+                    values={arnsSourceContract.records}
+                    value={domain}
+                    onSubmit={(next = false) => {
+                      dispatchRegisterState({
+                        type: 'setIsSearching',
+                        payload: true,
+                      });
+                      if (next) {
+                        dispatchRegisterState({
+                          type: 'setStage',
+                          payload: stage + 1,
+                        });
+                      }
+                    }}
+                    onChange={() => {
+                      dispatchRegisterState({
+                        type: 'reset',
+                      });
+                    }}
+                    onSuccess={(value: string) => {
+                      dispatchRegisterState({
+                        type: 'setDomainName',
+                        payload: value,
+                      });
+                      dispatchRegisterState({
+                        type: 'setAntID',
+                        payload: undefined,
+                      });
+                    }}
+                    onFailure={(name: string, result?: string) => {
+                      dispatchRegisterState({
+                        type: 'setDomainName',
+                        payload: name,
+                      });
+                      dispatchRegisterState({
+                        type: 'setAntID',
+                        payload: result
+                          ? new ArweaveTransactionID(result)
+                          : undefined,
+                      });
+                    }}
+                    successPredicate={(value: string | undefined) =>
+                      isArNSDomainNameAvailable({
+                        name: value,
+                        records: arnsSourceContract?.records ?? {},
+                      })
+                    }
+                    validationPredicate={(value: string | undefined) =>
+                      isArNSDomainNameValid({ name: value })
+                    }
+                    placeholderText={'Enter a name'}
+                    headerElement={
+                      <SearchBarHeader defaultText={'Find a domain name'} />
+                    }
+                    footerElement={
+                      <SearchBarFooter
+                        searchTerm={domain}
+                        searchResult={
+                          domain && arnsSourceContract.records[domain]
+                            ? new ArweaveTransactionID(
+                                arnsSourceContract.records[domain].contractTxId,
+                              )
+                            : undefined
+                        }
+                        defaultText="Names must be 1-32 characters. Dashes are permitted, but cannot be trailing characters and cannot be used in single character domains."
+                      />
+                    }
+                    height={45}
+                  />
+                ),
+                disableNext: !isSearching,
+                showNext:
+                  !!domain &&
+                  isArNSDomainNameAvailable({
+                    name: domain,
+                    records: arnsSourceContract.records,
+                  }) &&
+                  isArNSDomainNameValid({ name: domain }),
+                showBack: !!domain,
+                requiresWallet: !!domain && !antID,
+              },
+              1: {
+                component: <RegisterNameForm />,
+                showNext: true,
+                showBack: true,
+                disableNext:
+                  !antID ||
+                  !ARNS_TX_ID_REGEX.test(antID.toString()) ||
+                  !walletAddress,
+                requiresWallet: true,
+              },
+              2: {
+                // this component manages buttons itself
+                component: <ConfirmRegistration />,
+                showNext: false,
+                showBack: false,
+                disableNext: !!domain && !!antID && !walletAddress,
+                requiresWallet: true,
+              },
+              3: {
+                component: <SuccessfulRegistration />,
+                showNext: false,
+                showBack: false,
+                disableNext: true,
+                requiresWallet: true,
+              },
+            }}
+          />
+          {featuredDomains && !domain ? (
+            <FeaturedDomains domains={featuredDomains} />
+          ) : (
+            <></>
+          )}
+        </>
       )}
     </div>
   );
