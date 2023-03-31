3c8c7b76ea957dbc154a4b1f021750fe536ef871
dtfiedler <dylan@ar.io>
2023-03-31 13:35:58 -0600

    chore(cleanup): remove unnecessary calls to search params and navigate

--- src/components/layout/Navbar/Navbar.tsx
+++ src/components/layout/Navbar/Navbar.tsx
@@ -1,4 +1,4 @@
-import { Link, useNavigate, useSearchParams } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 import logo from '../../../../assets/images/logo/winston-white.gif';
 import { useRegistrationState } from '../../../state/contexts/RegistrationState';
@@ -6,10 +6,8 @@ import NavGroup from './NavGroup/NavGroup';
 import './styles.css';
 
 function NavBar() {
-  // eslint-disable-next-line
-  const [{}, dispatchRegisterState] = useRegistrationState();
-  const [, setSearchParams] = useSearchParams();
-  const navigate = useNavigate();
+  const [, dispatchRegisterState] = useRegistrationState();
+
   return (
     <div className="flex-row flex-space-between">
       <div className="flex-row flex-left">
@@ -17,9 +15,7 @@ function NavBar() {
           className="hover"
           to="/"
           onClick={() => {
-            setSearchParams();
             dispatchRegisterState({ type: 'reset' });
-            navigate('/');
           }}
         >
           <img src={logo} className="brand-logo" alt="logo" />
