7e14f99b4f762dbd9ac46b9942190396b8b20106
atticusofsparta <atticusofsparta@protonmail.com>
2023-04-10 07:26:39 -0600

    feat(PE-3359): added route for the transactio page

---
+++ src/components/pages/Transaction/Transaction.tsx
@@ -0,0 +1,5 @@
+function Transaction() {
+  return <></>;
+}
+
+export default Transaction;
--- src/components/pages/index.ts
+++ src/components/pages/index.ts
@@ -2,5 +2,6 @@ import About from './About/About';
 import Home from './Home/Home';
 import Manage from './Manage/Manage';
 import NotFound from './NotFound/NotFound';
+import Transaction from './Transaction/Transaction';
 
-export { About, Home, Manage, NotFound };
+export { About, Home, Manage, NotFound, Transaction };
