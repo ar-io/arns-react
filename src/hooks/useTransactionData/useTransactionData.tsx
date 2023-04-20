9fa04817e33a1db313f810e6853f6eae2208e50e
dtfiedler <dylan@ar.io>
2023-04-20 13:34:11 -0600

    fix(PE-3913): update useTransactionDataHook to set params based on TransactionState
    The way we had it, the TransactionState was updated based on URL params. This approach is very brittle, as a bad URL with the various type casting could easily break. Doing it this way, we can explicity set the TransactonState from anywhere in the app, and then update the URL accordingly. In a separate PR, we can add the logic that parses searchParams for transactionData values, and updates the TransactionState accordingly.

--- src/hooks/useTransactionData/useTransactionData.tsx
+++ src/hooks/useTransactionData/useTransactionData.tsx
@@ -1,55 +1,50 @@
-import { useEffect, useState } from 'react';
-import { useSearchParams } from 'react-router-dom';
+import { useEffect } from 'react';
+import {
+  createSearchParams,
+  useLocation,
+  useNavigate,
+  useSearchParams,
+} from 'react-router-dom';
 
-import { AntInteraction, ContractType, RegistryInteraction } from '../../types';
-import { getTransactionPayloadByInteractionType } from '../../utils/searchUtils';
+import { useTransactionState } from '../../state/contexts/TransactionState';
 
 function useTransactionData() {
-  const [searchParams] = useSearchParams();
-  const [URLTransactionData, setURLTransactionData] = useState<{
-    [x: string]: any;
-  }>({});
-  const [URLAssetId, setURLAssetId] = useState<string>(
-    () => searchParams.get('assetId') ?? '',
-  );
-  const [URLContractType, setURLContractType] = useState<ContractType>(
-    () =>
-      (searchParams.get('contractType') as ContractType) ??
-      ('' as ContractType),
-  );
-  const [URLInteractionType, setURLInteractionType] = useState<
-    AntInteraction | RegistryInteraction
-  >(
-    (searchParams.get('interactionType') as
-      | AntInteraction
-      | RegistryInteraction) ?? ('' as AntInteraction | RegistryInteraction),
-  );
+  const navigate = useNavigate();
+  const [, setSearchParams] = useSearchParams();
+  const from = useLocation().state;
+
+  const [{ transactionData, contractType, interactionType, workflowStage }] =
+    useTransactionState();
+
+  /**
+   * TODO: parse search params that are provided on initial page load and update the
+   * transaction state with those values. That will allow users to dynamically link
+   * transaction interactions.
+   */
 
   useEffect(() => {
-    const newURLAssetId = searchParams.get('assetId');
-    const newURLContractType = searchParams.get('contractType');
-    const newURLInteractionType = searchParams.get('interactionType');
-    const newURLTransactionData = searchParams.getAll('data');
-    setURLAssetId(newURLAssetId!);
-    setURLContractType(newURLContractType as ContractType);
-    setURLInteractionType(
-      newURLInteractionType as AntInteraction | RegistryInteraction,
-    );
-
-    if (newURLTransactionData) {
-      const payload = getTransactionPayloadByInteractionType(
-        URLContractType,
-        URLInteractionType,
-        newURLTransactionData,
-      );
-      if (payload) {
-        setURLTransactionData({ ...payload, assetId: URLAssetId });
-      }
+    if (!transactionData) {
+      navigate(from ?? '/');
+      return;
     }
-  }, [searchParams]);
-  // TODO: implement autofixes to type compatibility descrepencies ( if transaction data includes valid create ant keys for example, set the contract and interaction types appropriately)
 
-  return { URLTransactionData, URLContractType, URLInteractionType };
+    const updatedSearchParams = createSearchParams({
+      // TODO: sanitize these values
+      ...(transactionData as any),
+      contractType,
+      interactionType,
+      workflowStage,
+    });
+
+    setSearchParams(updatedSearchParams);
+  }, [transactionData, contractType, interactionType, workflowStage]);
+
+  return {
+    transactionData,
+    contractType,
+    interactionType,
+    workflowStage,
+  };
 }
 
 export default useTransactionData;
--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 import GlobalStateProvider from './state/contexts/GlobalState';
-import RegistrationStateProvider from './state/contexts/RegistrationState.js';
+import RegistrationStateProvider from './state/contexts/RegistrationState';
 import TransactionStateProvider from './state/contexts/TransactionState';
 import { reducer, registrationReducer } from './state/reducers';
 import { transactionReducer } from './state/reducers/TransactionReducer';
