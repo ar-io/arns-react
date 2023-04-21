9eaf7c11971f4d81ea81e458919b2d15034037bb
dtfiedler <dylan@ar.io>
2023-04-21 15:14:33 -0600

    chore: update ConfrimAntCreation component to use ANTContract class

--- src/components/layout/ConfirmAntCreation/ConfirmAntCreation.tsx
+++ src/components/layout/ConfirmAntCreation/ConfirmAntCreation.tsx
@@ -1,11 +1,23 @@
+import { useEffect, useState } from 'react';
+
+import { ANTContract } from '../../../services/arweave/AntContract';
 import { ANTContractJSON } from '../../../types';
 import { byteSize } from '../../../utils';
-import { DEFAULT_TTL_SECONDS, NAME_PRICE_INFO } from '../../../utils/constants';
+import { NAME_PRICE_INFO } from '../../../utils/constants';
 import { AntCard } from '../../cards';
 import ArPrice from '../ArPrice/ArPrice';
 import { Tooltip } from '../Tooltip/Tooltip';
 
 function ConfirmAntCreation({ state }: { state: ANTContractJSON }) {
+  const [ant, setAnt] = useState<ANTContract>();
+  useEffect(() => {
+    setAnt(new ANTContract(state));
+  }, [state]);
+
+  if (!ant) {
+    return <div>Something went wrong. Try again</div>;
+  }
+
   return (
     <div className="register-name-modal center">
       <AntCard
@@ -16,14 +28,8 @@ function ConfirmAntCreation({ state }: { state: ANTContractJSON }) {
         enableActions={false}
         showTier={false}
         overrides={{
-          targetId:
-            typeof state.records['@'] == 'string'
-              ? state.records['@']
-              : state.records['@'].transactionId,
-          ttlSeconds:
-            typeof state.records['@'] == 'string'
-              ? DEFAULT_TTL_SECONDS
-              : state.records['@'].ttlSeconds,
+          targetId: ant.getRecord('@').transactionId,
+          ttlSeconds: ant.getRecord('@').ttlSeconds,
         }}
         disabledKeys={[
           'tier',
