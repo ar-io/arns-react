7cf4faff7d4a1dfb36da40cf7b34072067e269a4
dtfiedler <dylan@ar.io>
2023-04-26 11:43:51 -0600

    fix: show loader on manage ant page

--- src/components/modals/ManageAntModal/ManageAntModal.tsx
+++ src/components/modals/ManageAntModal/ManageAntModal.tsx
@@ -23,6 +23,7 @@ import eventEmitter from '../../../utils/events';
 import { mapKeyToAttribute } from '../../cards/AntCard/AntCard';
 import { ArrowLeft, CloseIcon, PencilIcon } from '../../icons';
 import TransactionStatus from '../../layout/TransactionStatus/TransactionStatus';
+import { Loader } from '../../layout/index';
 
 const EDITABLE_FIELDS = [
   'name',
@@ -56,18 +57,15 @@ function ManageAntModal() {
   const [editingField, setEditingField] = useState<string>();
   const [modifiedValue, setModifiedValue] = useState<string>();
   const [rows, setRows] = useState<ManageAntRow[]>([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    try {
-      if (!id) {
-        throw Error('No id provided.');
-      }
-      const txId = new ArweaveTransactionID(id);
-      fetchAntDetails(txId);
-    } catch (error) {
-      eventEmitter.emit('error', error);
-      navigate('/manage');
+    if (!id) {
+      navigate('/manage/ants');
+      return;
     }
+    const txId = new ArweaveTransactionID(id);
+    fetchAntDetails(txId);
   }, [id]);
 
   function getAssociatedNames(txId: ArweaveTransactionID) {
@@ -79,51 +77,57 @@ function ManageAntModal() {
   }
 
   async function fetchAntDetails(txId: ArweaveTransactionID) {
-    const names = getAssociatedNames(txId);
-    const [contractState, confirmations] = await Promise.all([
-      arweaveDataProvider.getContractState<ANTContractJSON>(txId),
-      arweaveDataProvider.getTransactionStatus(txId),
-    ]);
-    const ant = new ANTContract(contractState);
-    const record = Object.values(arnsSourceContract.records).find(
-      (r) => r.contractTxId === txId.toString(),
-    );
-    const tier = arnsSourceContract.tiers.history.find(
-      (t) => t.id === record?.tier,
-    );
-    // TODO: add error messages and reload state to row
-    const consolidatedDetails: ManageAntRow & any = {
-      status: confirmations ?? 0,
-      associatedNames: !names.length ? 'N/A' : names.join(', '),
-      name: ant.name ?? 'N/A',
-      ticker: ant.ticker ?? 'N/A',
-      targetID: ant.getRecord('@').transactionId ?? 'N/A',
-      ttlSeconds: ant.getRecord('@').ttlSeconds,
-      controller:
-        contractState.controllers?.join(', ') ??
-        contractState.owner?.toString() ??
-        'N/A',
-      undernames: `${names.length} / ${tier?.settings.maxUndernames ?? 100}`,
-      owner: contractState.owner?.toString() ?? 'N/A',
-    };
+    try {
+      const names = getAssociatedNames(txId);
+      const [contractState, confirmations] = await Promise.all([
+        arweaveDataProvider.getContractState<ANTContractJSON>(txId),
+        arweaveDataProvider.getTransactionStatus(txId),
+      ]);
+      const ant = new ANTContract(contractState);
+      const record = Object.values(arnsSourceContract.records).find(
+        (r) => r.contractTxId === txId.toString(),
+      );
+      const tier = arnsSourceContract.tiers.history.find(
+        (t) => t.id === record?.tier,
+      );
+      // TODO: add error messages and reload state to row
+      const consolidatedDetails: ManageAntRow & any = {
+        status: confirmations ?? 0,
+        associatedNames: !names.length ? 'N/A' : names.join(', '),
+        name: ant.name ?? 'N/A',
+        ticker: ant.ticker ?? 'N/A',
+        targetID: ant.getRecord('@').transactionId ?? 'N/A',
+        ttlSeconds: ant.getRecord('@').ttlSeconds,
+        controller:
+          contractState.controllers?.join(', ') ??
+          contractState.owner?.toString() ??
+          'N/A',
+        undernames: `${names.length} / ${tier?.settings.maxUndernames ?? 100}`,
+        owner: contractState.owner?.toString() ?? 'N/A',
+      };
 
-    const rows = Object.keys(consolidatedDetails).reduce(
-      (details: ManageAntRow[], attribute: string, index: number) => {
-        const detail = {
-          attribute,
-          value: consolidatedDetails[attribute as keyof ManageAntRow],
-          editable: EDITABLE_FIELDS.includes(attribute),
-          action: ACTION_FIELDS[attribute] ?? undefined, // navigate to transaction route with details
-          key: index,
-          interactionType: getInteractionTypeFromField(attribute),
-        };
-        details.push(detail);
-        return details;
-      },
-      [],
-    );
-    setAntName(contractState.name ?? id);
-    setRows(rows);
+      const rows = Object.keys(consolidatedDetails).reduce(
+        (details: ManageAntRow[], attribute: string, index: number) => {
+          const detail = {
+            attribute,
+            value: consolidatedDetails[attribute as keyof ManageAntRow],
+            editable: EDITABLE_FIELDS.includes(attribute),
+            action: ACTION_FIELDS[attribute] ?? undefined, // navigate to transaction route with details
+            key: index,
+            interactionType: getInteractionTypeFromField(attribute),
+          };
+          details.push(detail);
+          return details;
+        },
+        [],
+      );
+      setAntName(contractState.name ?? id);
+      setRows(rows);
+      setLoading(false);
+    } catch (error) {
+      eventEmitter.emit('error', error);
+      navigate('/manage');
+    }
   }
 
   return (
@@ -161,163 +165,177 @@ function ManageAntModal() {
           <CloseIcon width="30px" height={'30px'} fill="var(--text-white)" />
         </button>
       </div>
-      <div className="flex-row">
-        <Table
-          showHeader={false}
-          style={{ width: '100%' }}
-          onRow={(row: ManageAntRow) => ({
-            className: row.attribute === editingField ? 'active-row' : '',
-          })}
-          scroll={{ x: true }}
-          columns={[
-            {
-              title: '',
-              dataIndex: 'attribute',
-              key: 'attribute',
-              align: 'left',
-              width: isMobile ? '0px' : '30%',
-              className: 'icon-padding white',
-              render: (value: string) => {
-                return `${mapKeyToAttribute(value)}:`;
+      <div className="flex-row center">
+        {loading ? (
+          <div className="flex" style={{ padding: '10%' }}>
+            <Loader size={80} />
+          </div>
+        ) : (
+          <Table
+            showHeader={false}
+            style={{ width: '100%' }}
+            onRow={(row: ManageAntRow) => ({
+              className: row.attribute === editingField ? 'active-row' : '',
+            })}
+            scroll={{ x: true }}
+            columns={[
+              {
+                title: '',
+                dataIndex: 'attribute',
+                key: 'attribute',
+                align: 'left',
+                width: isMobile ? '0px' : '30%',
+                className: 'icon-padding white',
+                render: (value: string) => {
+                  return `${mapKeyToAttribute(value)}:`;
+                },
               },
-            },
-            {
-              title: '',
-              dataIndex: 'value',
-              key: 'value',
-              align: 'left',
-              width: '80%',
-              className: 'white',
-              render: (value: string | number, row: any) => {
-                if (row.attribute === 'status')
-                  return (
-                    <>
-                      {/* TODO: add label for mobile view */}
-                      <TransactionStatus confirmations={+value} />
-                    </>
-                  );
-                if (row.editable)
-                  return (
-                    <>
-                      {/* TODO: add label for mobile view */}
-                      <input
-                        id={row.attribute}
-                        style={{
-                          width: '80%',
-                          fontSize: '16px',
-                          background:
-                            editingField === row.attribute
-                              ? 'white'
-                              : 'transparent',
-                          border:
-                            editingField === row.attribute
-                              ? '2px solid #E0E0E0'
-                              : 'none',
-                          borderRadius: '2px',
-                          color:
-                            editingField === row.attribute ? 'black' : 'white',
-                          padding:
-                            editingField === row.attribute
-                              ? '10px '
-                              : '10px 0px',
-                          display: 'block',
-                        }}
-                        disabled={editingField !== row.attribute}
-                        value={
-                          editingField !== row.attribute ? value : modifiedValue
-                        }
-                        onChange={(e) => setModifiedValue(e.target.value)}
-                      />
-                    </>
-                  );
-                return value;
-              },
-            },
-            {
-              title: '',
-              dataIndex: 'action',
-              key: 'action',
-              width: '10%',
-              align: 'right',
-              className: 'white',
-              render: (value: any, row: any) => {
-                //TODO: if it's got an action attached, show it
-                if (row.editable) {
-                  return (
-                    <>
-                      {editingField !== row.attribute ? (
-                        <button
-                          onClick={() => {
-                            setEditingField(row.attribute);
-                            setModifiedValue(row.value);
-                          }}
-                        >
-                          <PencilIcon
-                            style={{ width: '24', height: '24', fill: 'white' }}
-                          />
-                        </button>
-                      ) : (
-                        <button
-                          className="assets-manage-button"
+              {
+                title: '',
+                dataIndex: 'value',
+                key: 'value',
+                align: 'left',
+                width: '80%',
+                className: 'white',
+                render: (value: string | number, row: any) => {
+                  if (row.attribute === 'status')
+                    return (
+                      <>
+                        {/* TODO: add label for mobile view */}
+                        <TransactionStatus confirmations={+value} />
+                      </>
+                    );
+                  if (row.editable)
+                    return (
+                      <>
+                        {/* TODO: add label for mobile view */}
+                        <input
+                          id={row.attribute}
                           style={{
-                            backgroundColor: 'var(--accent)',
-                            borderColor: 'var(--accent)',
+                            width: '80%',
+                            fontSize: '16px',
+                            background:
+                              editingField === row.attribute
+                                ? 'white'
+                                : 'transparent',
+                            border:
+                              editingField === row.attribute
+                                ? '2px solid #E0E0E0'
+                                : 'none',
+                            borderRadius: '2px',
+                            color:
+                              editingField === row.attribute
+                                ? 'black'
+                                : 'white',
+                            padding:
+                              editingField === row.attribute
+                                ? '10px '
+                                : '10px 0px',
+                            display: 'block',
                           }}
-                          onClick={() => {
-                            const payload =
-                              getTransactionPayloadByInteractionType(
-                                CONTRACT_TYPES.ANT,
-                                row.interactionType,
-                                modifiedValue!,
-                              );
-                            if (payload && row.interactionType && id) {
-                              // eslint-disable-next-line
-                              const { assetId, functionName, ...data } =
-                                payload;
-                              dispatchTransactionState({
-                                type: 'setContractType',
-                                payload: CONTRACT_TYPES.ANT,
-                              });
-                              dispatchTransactionState({
-                                type: 'setInteractionType',
-                                payload: row.interactionType,
-                              });
-                              dispatchTransactionState({
-                                type: 'setTransactionData',
-                                payload: {
-                                  assetId: id,
-                                  functionName,
-                                  ...data,
-                                },
-                              });
-                              navigate(`/transaction`, {
-                                state: `/manage/ants/${id}`,
-                              });
-                            }
-                          }}
-                        >
-                          Save
-                        </button>
-                      )}
-                    </>
-                  );
-                }
-                if (row.action) {
-                  return (
-                    <button
-                      onClick={row.action.fn}
-                      className="assets-manage-button"
-                    >
-                      {row.action.title}
-                    </button>
-                  );
-                }
-                return value;
+                          disabled={editingField !== row.attribute}
+                          value={
+                            editingField !== row.attribute
+                              ? value
+                              : modifiedValue
+                          }
+                          onChange={(e) => setModifiedValue(e.target.value)}
+                        />
+                      </>
+                    );
+                  return value;
+                },
               },
-            },
-          ]}
-          data={rows}
-        />
+              {
+                title: '',
+                dataIndex: 'action',
+                key: 'action',
+                width: '10%',
+                align: 'right',
+                className: 'white',
+                render: (value: any, row: any) => {
+                  //TODO: if it's got an action attached, show it
+                  if (row.editable) {
+                    return (
+                      <>
+                        {editingField !== row.attribute ? (
+                          <button
+                            onClick={() => {
+                              setEditingField(row.attribute);
+                              setModifiedValue(row.value);
+                            }}
+                          >
+                            <PencilIcon
+                              style={{
+                                width: '24',
+                                height: '24',
+                                fill: 'white',
+                              }}
+                            />
+                          </button>
+                        ) : (
+                          <button
+                            className="assets-manage-button"
+                            style={{
+                              backgroundColor: 'var(--accent)',
+                              borderColor: 'var(--accent)',
+                            }}
+                            onClick={() => {
+                              const payload =
+                                getTransactionPayloadByInteractionType(
+                                  CONTRACT_TYPES.ANT,
+                                  row.interactionType,
+                                  modifiedValue!,
+                                );
+                              if (payload && row.interactionType && id) {
+                                // eslint-disable-next-line
+                                const { assetId, functionName, ...data } =
+                                  payload;
+                                dispatchTransactionState({
+                                  type: 'setContractType',
+                                  payload: CONTRACT_TYPES.ANT,
+                                });
+                                dispatchTransactionState({
+                                  type: 'setInteractionType',
+                                  payload: row.interactionType,
+                                });
+                                dispatchTransactionState({
+                                  type: 'setTransactionData',
+                                  payload: {
+                                    assetId: id,
+                                    functionName,
+                                    ...data,
+                                  },
+                                });
+                                navigate(`/transaction`, {
+                                  state: `/manage/ants/${id}`,
+                                });
+                              }
+                            }}
+                          >
+                            Save
+                          </button>
+                        )}
+                      </>
+                    );
+                  }
+                  if (row.action) {
+                    return (
+                      <button
+                        onClick={row.action.fn}
+                        className="assets-manage-button"
+                      >
+                        {row.action.title}
+                      </button>
+                    );
+                  }
+                  return value;
+                },
+              },
+            ]}
+            data={rows}
+          />
+        )}
       </div>
     </div>
   );
