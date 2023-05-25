9e7df701ebc62436804fff09180b153cdbd19e82
dtfiedler <dylan@ar.io>
2023-05-25 09:29:12 -0600

    chore(hash): add hash to footer version so its obvious what version is deployed

--- src/components/layout/Footer/Footer.tsx
+++ src/components/layout/Footer/Footer.tsx
@@ -2,7 +2,8 @@ function Footer() {
   return (
     <div className="flex-row flex-center">
       <div className="text faded center">
-        v{process.env.npm_package_version}
+        v{process.env.npm_package_version}-
+        {process.env.VITE_GITHUB_HASH?.slice(0, 6)}
         <br />
         <br />
         Copyright 2021 - {new Date().getFullYear()}
