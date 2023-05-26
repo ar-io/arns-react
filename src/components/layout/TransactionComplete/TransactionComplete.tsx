b149b38dec15e48cd9d24054ca0621645f77c9f6
atticusofsparta <atticusofsparta@protonmail.com>
2023-05-26 08:09:38 -0600

    fixit(PE-3065): remove sonar link

--- src/components/layout/TransactionComplete/TransactionComplete.tsx
+++ src/components/layout/TransactionComplete/TransactionComplete.tsx
@@ -2,14 +2,9 @@ import { Link, useNavigate } from 'react-router-dom';
 
 import {
   ArweaveTransactionID,
-  INTERACTION_TYPES,
   TransactionData,
   ValidInteractionType,
 } from '../../../types';
-import {
-  WARP_CONTRACT_BASE_URL,
-  WARP_INTERACTION_BASE_URL,
-} from '../../../utils/constants';
 import eventEmitter from '../../../utils/events';
 import {
   getLinkId,
@@ -41,13 +36,20 @@ function TransactionComplete({
 
   return (
     <>
-      <div className="flex-column center" style={{ gap: '3em' }}>
+      <div
+        className="flex-column center"
+        style={{ gap: '3em', width: '700px' }}
+      >
         <div className="flex-column center" style={{ gap: '2em' }}>
           {/* TODO: configure error or fail states */}
           <PDNTCard {...pdntProps} />
           <div
             className="flex flex-row center"
-            style={{ gap: '1em', maxWidth: '782px' }}
+            style={{
+              maxWidth: '75%',
+              justifyContent: 'space-evenly',
+              boxSizing: 'border-box',
+            }}
           >
             <Link to="/" className="link" style={{ textDecoration: 'none' }}>
               <div
@@ -68,42 +70,6 @@ function TransactionComplete({
                 />
                 <span className="flex text-medium faded center">
                   Register a Name
-                </span>
-              </div>
-            </Link>
-
-            <Link
-              rel="noreferrer"
-              target={'_blank'}
-              to={
-                interactionType === INTERACTION_TYPES.CREATE
-                  ? WARP_CONTRACT_BASE_URL
-                  : WARP_INTERACTION_BASE_URL + transactionId?.toString()
-              }
-              className="link"
-              style={{ textDecoration: 'none' }}
-            >
-              <div
-                className="flex flex-column center card"
-                style={{
-                  minWidth: '175px',
-                  minHeight: '100px',
-                  flex: 1,
-                  padding: '0px',
-                  gap: '.5em',
-                  textDecoration: 'none',
-                }}
-              >
-                <ArrowUpRight
-                  width={'30px'}
-                  height={'30px'}
-                  fill={'var(--text-white)'}
-                />
-                <span
-                  className="flex text-medium faded center"
-                  style={{ textDecorationLine: 'none' }}
-                >
-                  View on Sonar
                 </span>
               </div>
             </Link>
