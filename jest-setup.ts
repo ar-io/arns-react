6e40b8a0b2df0a64a5af09ad9ed8a9662bbad189
atticusofsparta <atticusofsparta@protonmail.com>
2023-06-28 20:17:53 -0600

    tests: added notes to jest setup

--- jest-setup.ts
+++ jest-setup.ts
@@ -3,11 +3,12 @@
 import '@testing-library/jest-dom';
 import 'core-js';
 import 'jest-location-mock';
+// was needed for github actions environment
 import matchMediaMock from 'match-media-mock';
 import React from 'react';
 import { TextDecoder, TextEncoder } from 'util';
 
-window.matchMedia ??= matchMediaMock.create();
+window.matchMedia ??= matchMediaMock.create(); // required for rendering ant design components, specifically at time of addition, the steps component.
 
 global.React = React;
 global.TextEncoder = TextEncoder;
