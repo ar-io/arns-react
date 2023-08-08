bb68b2e20af8c390a43f8035bf3379ac336130c4
atticusofsparta <atticusofsparta@protonmail.com>
2023-08-08 15:04:34 -0600

    chore(tests): add tests for searchbar

--- src/components/inputs/Search/SearchBar/__tests__/SearchBar.test.tsx
+++ src/components/inputs/Search/SearchBar/__tests__/SearchBar.test.tsx
@@ -1,37 +1,76 @@
 import { act, cleanup, render } from '@testing-library/react';
 import { HashRouter as Router } from 'react-router-dom';
-
+import userEvent from '@testing-library/user-event';
 import { SearchBarFooter, SearchBarHeader } from '../../../../layout';
 import SearchBar from '../SearchBar';
 
+
+const TEST_RECORDS = {
+  "ardrive":{
+    contractTxId:"I-cxQhfh0Zb9UqQNizC9PiLC41KpUeA9hjiVV02rQRw",
+    endTimestamp:1711122739,
+    tier:"SEC0-8cTfyDBRQo21KNIhUV5KreuEmIY05wX-VOeESE",
+    type:"lease"
+  },
+  "xn--go8h6v":{
+    contractTxId:"I-cxQhfh0Zb9UqQNizC9PiLC41KpUeA9hjiVV02rQRw",
+    endTimestamp:1711122739,
+    tier:"SEC0-8cTfyDBRQo21KNIhUV5KreuEmIY05wX-VOeESE",
+    type:"lease"
+  }
+}
+
 describe('SearchBar', () => {
   afterEach(cleanup);
+  const onChange = jest.fn();
+  const onSubmit = jest.fn();
+  const onFailure = jest.fn();
+  const onSuccess = jest.fn();
+  // Other stubs as needed...
+  const stub = jest.fn();
 
-  test('render SearchBar', async () => {
-    const stub = jest.fn();
-    await act(async () =>
-      render(
-        <Router>
-          <SearchBar
-            onSubmit={stub}
-            onChange={stub}
-            onFailure={stub}
-            onSuccess={stub}
-            successPredicate={stub}
-            validationPredicate={stub}
-            value=""
-            values={{}}
-            placeholderText={'Find a name'}
-            headerElement={
-              <SearchBarHeader defaultText="Find a name" reservedList={[]} />
-            }
-            footerElement={
-              <SearchBarFooter isAuction={false} reservedList={[]} />
-            }
-          />
-          ,
-        </Router>,
-      ),
+  test('handles a search with capitalized names correctly', async () => {
+
+
+    const { getByTestId } = render(
+      <Router>
+            <SearchBar
+              onSubmit={onSubmit}
+              onChange={onChange}
+              onFailure={onFailure}
+              onSuccess={onSuccess}
+              successPredicate={stub}
+              validationPredicate={stub}
+              value=""
+              values={TEST_RECORDS}
+              placeholderText={'Find a name'}
+              headerElement={
+                <SearchBarHeader defaultText="Find a name" reservedList={[]} />
+              }
+              footerElement={
+                <SearchBarFooter isAuction={false} reservedList={[]} />
+              }
+            />
+      </Router>,
     );
+  
+    const searchInput = getByTestId("searchbar-input-id");
+    const searchButton = getByTestId('search-button'); // Assuming you used data-testid="search-button"
+    const domain = "ARDRIVE"
+  
+    await userEvent.type(searchInput, domain);
+    userEvent.click(searchButton);
+  
+    expect(onChange).toHaveBeenCalledWith(domain);
+    expect(onSubmit).toHaveBeenCalled();
+    expect(onFailure).toHaveBeenCalled()
+    // Additional assertions as needed...
   });
+
+
 });
+
+
+
+
+
