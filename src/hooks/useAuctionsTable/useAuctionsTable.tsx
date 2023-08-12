2391ded284963a449c1f8d90afc1d0513316a365
atticusofsparta <atticusofsparta@protonmail.com>
2023-08-11 18:38:44 -0600

    feat(auctions page): update routing to register page

--- src/hooks/useAuctionsTable/useAuctionsTable.tsx
+++ src/hooks/useAuctionsTable/useAuctionsTable.tsx
@@ -284,7 +284,7 @@ export function useAuctionsTable() {
               className="accent-button hover"
               style={{ fontSize: '13px', padding: '8px' }}
               onClick={() => {
-                alert('Setup registration page to enable this button');
+                navigate(`/register/${row.name}`);
               }}
             >
               Buy
