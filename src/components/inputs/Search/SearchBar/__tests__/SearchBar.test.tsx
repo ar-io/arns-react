b0b33463bad2b4a510e1f2a407ab4d8d5f03dcdb
atticusofsparta <atticusofsparta@protonmail.com>
2023-08-14 18:35:21 -0600

    chore(formatting): format

--- src/components/inputs/Search/SearchBar/__tests__/SearchBar.test.tsx
+++ src/components/inputs/Search/SearchBar/__tests__/SearchBar.test.tsx
@@ -1,24 +1,24 @@
 import { act, cleanup, render } from '@testing-library/react';
-import { HashRouter as Router } from 'react-router-dom';
 import userEvent from '@testing-library/user-event';
+import { HashRouter as Router } from 'react-router-dom';
+
 import { SearchBarFooter, SearchBarHeader } from '../../../../layout';
 import SearchBar from '../SearchBar';
 
-
 const TEST_RECORDS = {
-  "ardrive":{
-    contractTxId:"I-cxQhfh0Zb9UqQNizC9PiLC41KpUeA9hjiVV02rQRw",
-    endTimestamp:1711122739,
-    tier:"SEC0-8cTfyDBRQo21KNIhUV5KreuEmIY05wX-VOeESE",
-    type:"lease"
+  ardrive: {
+    contractTxId: 'I-cxQhfh0Zb9UqQNizC9PiLC41KpUeA9hjiVV02rQRw',
+    endTimestamp: 1711122739,
+    tier: 'SEC0-8cTfyDBRQo21KNIhUV5KreuEmIY05wX-VOeESE',
+    type: 'lease',
   },
-  "xn--go8h6v":{
-    contractTxId:"I-cxQhfh0Zb9UqQNizC9PiLC41KpUeA9hjiVV02rQRw",
-    endTimestamp:1711122739,
-    tier:"SEC0-8cTfyDBRQo21KNIhUV5KreuEmIY05wX-VOeESE",
-    type:"lease"
-  }
-}
+  'xn--go8h6v': {
+    contractTxId: 'I-cxQhfh0Zb9UqQNizC9PiLC41KpUeA9hjiVV02rQRw',
+    endTimestamp: 1711122739,
+    tier: 'SEC0-8cTfyDBRQo21KNIhUV5KreuEmIY05wX-VOeESE',
+    type: 'lease',
+  },
+};
 
 describe('SearchBar', () => {
   afterEach(cleanup);
@@ -30,47 +30,38 @@ describe('SearchBar', () => {
   const stub = jest.fn();
 
   test('handles a search with capitalized names correctly', async () => {
-
-
     const { getByTestId } = render(
       <Router>
-            <SearchBar
-              onSubmit={onSubmit}
-              onChange={onChange}
-              onFailure={onFailure}
-              onSuccess={onSuccess}
-              successPredicate={stub}
-              validationPredicate={stub}
-              value=""
-              values={TEST_RECORDS}
-              placeholderText={'Find a name'}
-              headerElement={
-                <SearchBarHeader defaultText="Find a name" reservedList={[]} />
-              }
-              footerElement={
-                <SearchBarFooter isAuction={false} reservedList={[]} />
-              }
-            />
+        <SearchBar
+          onSubmit={onSubmit}
+          onChange={onChange}
+          onFailure={onFailure}
+          onSuccess={onSuccess}
+          successPredicate={stub}
+          validationPredicate={stub}
+          value=""
+          values={TEST_RECORDS}
+          placeholderText={'Find a name'}
+          headerElement={
+            <SearchBarHeader defaultText="Find a name" reservedList={[]} />
+          }
+          footerElement={
+            <SearchBarFooter isAuction={false} reservedList={[]} />
+          }
+        />
       </Router>,
     );
-  
-    const searchInput = getByTestId("searchbar-input-id");
+
+    const searchInput = getByTestId('searchbar-input-id');
     const searchButton = getByTestId('search-button'); // Assuming you used data-testid="search-button"
-    const domain = "ARDRIVE"
-  
+    const domain = 'ARDRIVE';
+
     await userEvent.type(searchInput, domain);
     userEvent.click(searchButton);
-  
+
     expect(onChange).toHaveBeenCalledWith(domain);
     expect(onSubmit).toHaveBeenCalled();
-    expect(onFailure).toHaveBeenCalled()
+    expect(onFailure).toHaveBeenCalled();
     // Additional assertions as needed...
   });
-
-
 });
-
-
-
-
-
