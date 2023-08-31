468b801936812354216227ba0edcd41a57cb2e59
atticusofsparta <atticusofsparta@protonmail.com>
2023-08-30 18:25:47 -0600

    fix(addresses): redundant logic for wallet connect in hook

--- src/hooks/useWalletAddress/useWalletAddress.tsx
+++ src/hooks/useWalletAddress/useWalletAddress.tsx
@@ -1,44 +1,11 @@
-import { useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
-
 import { useGlobalState } from '../../state/contexts/GlobalState';
 import { ArweaveTransactionID } from '../../types';
-import eventEmitter from '../../utils/events';
 
 export function useWalletAddress(): {
   wallet: any;
   walletAddress: ArweaveTransactionID | undefined;
 } {
-  const navigate = useNavigate();
-  const [{ wallet, walletAddress }, dispatchGlobalState] = useGlobalState();
-
-  useEffect(() => {
-    if (!wallet) {
-      // remove address if wallet gets cleared
-      dispatchGlobalState({
-        type: 'setWalletAddress',
-        payload: undefined,
-      });
-      return;
-    }
-
-    wallet
-      .getWalletAddress()
-      .then((address: ArweaveTransactionID) => {
-        dispatchGlobalState({
-          type: 'setWalletAddress',
-          payload: address,
-        });
-      })
-      .catch((error: Error) => {
-        dispatchGlobalState({
-          type: 'setWalletAddress',
-          payload: undefined,
-        });
-        eventEmitter.emit('error', error);
-        navigate('/connect');
-      });
-  }, [wallet]);
+  const [{ wallet, walletAddress }] = useGlobalState();
 
   return {
     walletAddress,
