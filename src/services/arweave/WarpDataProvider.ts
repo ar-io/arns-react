4d170f0502d14f25ac3574d28d4d8230e2476959
atticusofsparta <atticusofsparta@protonmail.com>
2024-01-10 16:47:32 -0600

    fix(flow): reduce complexity of control flow check on if statement

--- src/services/arweave/WarpDataProvider.ts
+++ src/services/arweave/WarpDataProvider.ts
@@ -137,7 +137,7 @@ export class WarpDataProvider implements SmartweaveContractInteractionProvider {
       // because we are manually constructing the tags, we want to verify them immediately and always
       // an undefined valid means the transaction is valid
 
-      if (dryWriteResults && dryWriteResults?.type === 'error') {
+      if (dryWriteResults?.type === 'error') {
         throw new Error(
           `Contract interaction detected to be invalid: ${
             dryWriteResults?.originalErrorMessages
