1758c63960ae7281d9689abb55616c647a6bc232
atticusofsparta <atticusofsparta@protonmail.com>
2024-11-06 18:01:41 -0600

    fix(files): remove old register file

--- register.mjs
+++
@@ -1,5 +0,0 @@
-import { register } from 'node:module';
-import { pathToFileURL } from 'node:url';
-
-// Register ts-node as the loader with ESM support
-register('ts-node/esm', pathToFileURL('./'));
