fb6ff4cb2ca616122b7253290564f3c2f3f54533
atticusofsparta <atticusofsparta@protonmail.com>
2025-01-09 13:54:24 -0600

    fix(switch): use rem instead of px

--- src/components/inputs/Switch.tsx
+++ src/components/inputs/Switch.tsx
@@ -18,13 +18,13 @@ const Switch = ({
     checked={checked}
     className={
       className?.root ??
-      'relative h-[25px] w-[42px] cursor-default rounded-full bg-blackA6 shadow-[0_2px_10px] shadow-blackA4 outline-none focus:shadow-[0_0_0_2px] focus:shadow-black data-[state=checked]:bg-black'
+      'relative h-[1.5625rem] w-[2.625rem] cursor-default rounded-full bg-blackA6 shadow-[0_0.125rem_0.625rem] shadow-blackA4 outline-none focus:shadow-[0_0_0_0.125rem] focus:shadow-black data-[state=checked]:bg-black'
     }
   >
     <RadixSwitch.Thumb
       className={
         className?.thumb ??
-        'block size-[21px] translate-x-0.5 rounded-full bg-white shadow-[0_2px_2px] shadow-blackA4 transition-transform duration-100 will-change-transform data-[state=checked]:translate-x-[19px]'
+        'block size-[1.3125rem] translate-x-0.03125rem rounded-full bg-white shadow-[0_0.125rem_0.125rem] shadow-blackA4 transition-transform duration-100 will-change-transform data-[state=checked]:translate-x-[1.1875rem]'
       }
     />
   </RadixSwitch.Root>
