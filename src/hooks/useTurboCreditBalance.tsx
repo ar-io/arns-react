1df91e143661da04e55f1e9b6ee033bfbb309df7
atticusofsparta <atticusofsparta@protonmail.com>
2025-04-16 15:47:39 -0600

    fix(arweave app): dont put client in query key

--- src/hooks/useTurboCreditBalance.tsx
+++ src/hooks/useTurboCreditBalance.tsx
@@ -1,15 +1,16 @@
-import { useWalletState } from '@src/state';
+import { useGlobalState, useWalletState } from '@src/state';
 import { useQuery } from '@tanstack/react-query';
 
 import { useTurboArNSClient } from './useTurboArNSClient';
 
 export function useTurboCreditBalance(address?: string) {
   const [{ walletAddress }] = useWalletState();
+  const [{ turboNetwork }] = useGlobalState();
   const userAddress = address ?? walletAddress?.toString();
   const turboArNSClient = useTurboArNSClient();
 
   return useQuery({
-    queryKey: ['turbo-credit-balance', userAddress, turboArNSClient],
+    queryKey: ['turbo-credit-balance', userAddress, turboNetwork],
     queryFn: async () => {
       if (!userAddress) throw new Error('No wallet address found');
       return await turboArNSClient?.turboUploader.getBalance(
