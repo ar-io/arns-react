808f4aca79ea1c7b2439acf39fb68873292f7250
atticusofsparta <atticusofsparta@protonmail.com>
2025-08-04 09:11:55 -0600

    fix(turbo credit topup): do not use class as hash key for use query

--- src/hooks/useUploadCostGib.ts
+++ src/hooks/useUploadCostGib.ts
@@ -1,17 +1,26 @@
+import { useGlobalState } from '@src/state';
 import { GiB } from '@src/utils/constants';
 import { useQuery } from '@tanstack/react-query';
 
 import { useTurboArNSClient } from './useTurboArNSClient';
 
 const useUploadCostGib = () => {
+  const [{ turboNetwork }] = useGlobalState();
   const turbo = useTurboArNSClient();
   const res = useQuery({
-    queryKey: ['uploadCostGib', turbo],
-    queryFn: () => {
+    queryKey: ['uploadCostGib', turboNetwork],
+    queryFn: async () => {
       if (!turbo) {
         throw Error('TurboUnauthenticatedClient is not set');
       }
-      return turbo.turboUploader.getUploadCosts({ bytes: [GiB] });
+      const res = await turbo.turboUploader
+        .getUploadCosts({ bytes: [GiB] })
+        .catch((err) => {
+          console.log('err', err);
+          return null;
+        });
+      console.log('res', res);
+      return res;
     },
   });
   return res;
